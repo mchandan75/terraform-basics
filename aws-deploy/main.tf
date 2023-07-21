8d329c2aa8715ffb66ea34a54b92a1dbf276057a
mchandan75 <71778800+mchandan75@users.noreply.github.com>
2023-07-21 15:35:59 +0530

    ami id changed in main.tf

--- aws-deploy/main.tf
+++ aws-deploy/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "web" {
 
-  ami           = "ami-25488752"
+  ami           = "ami-05548f9cecf47b442"
   instance_type = "t2.micro"
   security_groups = [aws_security_group.ssh_http.name]
   user_data              = file("template/user_data.sh")
